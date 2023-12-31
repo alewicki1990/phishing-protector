f6e7cb745083a765881c2082ae62cc7970e58610
alewicki1990 <alewicki1990@gmail.com>
2023-12-31 21:27:57 +0100

    Empty project template

---
+++ src/main/java/com/github/lewicki1990/phishingprotector/PhishingProtectorApplication.java
@@ -0,0 +1,13 @@
+package com.github.lewicki1990.phishingprotector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhishingProtectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhishingProtectorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/lewicki1990/phishingprotector/PhishingProtectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.lewicki1990.phishingprotector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhishingProtectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
