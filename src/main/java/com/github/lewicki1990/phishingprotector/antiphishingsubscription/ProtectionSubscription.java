24781acf7ba505ca620b997fca2dec0756f508d1
alewicki1990 <alewicki1990@gmail.com>
2024-01-04 21:34:31 +0100

    Setting full import paths. Deleting these ended with asterisks.

--- src/main/java/com/github/lewicki1990/phishingprotector/antiphishingsubscription/ProtectionSubscription.java
+++ src/main/java/com/github/lewicki1990/phishingprotector/antiphishingsubscription/ProtectionSubscription.java
@@ -1,9 +1,18 @@
 package com.github.lewicki1990.phishingprotector.antiphishingsubscription;
 
-import jakarta.persistence.*;
-import lombok.*;
 
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "protection_subscription")
--- src/main/java/com/github/lewicki1990/phishingprotector/smsprocessing/lastfetchedsmsid/LastFetchedSmsId.java
+++ src/main/java/com/github/lewicki1990/phishingprotector/smsprocessing/lastfetchedsmsid/LastFetchedSmsId.java
@@ -1,7 +1,15 @@
 package com.github.lewicki1990.phishingprotector.smsprocessing.lastfetchedsmsid;
 
-import jakarta.persistence.*;
-import lombok.*;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Table(name="last_fetched_sms_id")
