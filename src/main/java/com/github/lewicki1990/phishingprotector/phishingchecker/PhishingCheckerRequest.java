22c53482c756dcdffd004fded1dd4ee066f60434
alewicki1990 <alewicki1990@gmail.com>
2024-01-04 23:22:02 +0100

    Request and response classes for link phishing check added.

---
+++ src/main/java/com/github/lewicki1990/phishingprotector/phishingchecker/PhishingCheckerRequest.java
@@ -0,0 +1,14 @@
+package com.github.lewicki1990.phishingprotector.phishingchecker;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PhishingCheckerRequest {
+
+    private String uri;
+
+}
