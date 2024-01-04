f2dbc4a5a2004b0153705710f7744016a805856b
alewicki1990 <alewicki1990@gmail.com>
2024-01-04 21:11:25 +0100

    new method transformSmsListToSmsDTOList(List<Sms> smses) added to interface

--- src/main/java/com/github/lewicki1990/phishingprotector/smssource/mapping/SmsMapper.java
+++ src/main/java/com/github/lewicki1990/phishingprotector/smssource/mapping/SmsMapper.java
@@ -3,6 +3,8 @@ package com.github.lewicki1990.phishingprotector.smssource.mapping;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.github.lewicki1990.phishingprotector.smssource.Sms;
 
+import java.util.List;
+
 public interface SmsMapper {
     SmsDTO convertSmsToSmsDTO(Sms sms) throws JsonProcessingException;
 
@@ -11,5 +13,7 @@ public interface SmsMapper {
     Sms convertSmsDTOToSMS(SmsDTO smsDTO) throws JsonProcessingException;
 
     String transformSmsAttributesIntoJson(SmsDTO smsDTO) throws JsonProcessingException;
+
+    List<SmsDTO> transformSmsListToSmsDTOList(List<Sms> smses);
 
 }
